283f0a75e099e496a2d0426af088b8dfb4ebdb5a
אפרת כהן <efratc66@gmail.com>
2018-05-21 11:01:47 +0300

    models

--- Maonot/Models/Message.cs
+++ Maonot/Models/Message.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
 
 namespace Maonot.Models
 {
     public class Message
     {
+        
+        public int MessageID { set; get; }
+        [Required]
+        public string Addressee { set; get; }
+        [Required]
+        public string Subject { set; get; }
+        [Required]
+        public string Content { set; get; }
+
+
+
+
+
     }
+    
 }
---
+++ Maonot/Models/Warning.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace Maonot.Models
+{
+    public class Warning
+       
+    {
+        public int WarningId { get; set; }
+        [Required]
+        public int WarningNumber { get; set; }
+        [Required]
+        public int StudentId { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+
+
+
+    }
+}
