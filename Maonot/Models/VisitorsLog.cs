68ee6cf38b6b7e5c9a9e8f09f68635261aea5dde
אפרת כהן <efratc66@gmail.com>
2018-05-21 11:14:27 +0300

    models

--- Maonot/Models/VisitorsLog.cs
+++ Maonot/Models/VisitorsLog.cs
@@ -2,10 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
 
 namespace Maonot.Models
+
 {
+    public enum RoomNum
+    {
+        אחת, שתיים,שלוש,ארבע
+    }
     public class VisitorsLog
     {
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EnteryDate { get; set; }
+        [Required]
+        public string VistorName { get; set; }
+        [Required]
+        public int VisitorID { get; set; }
+        
+        [DataType(DataType.Date)]
+        public DateTime ExitDate { get; set; }
+        [Required]
+        public int ApartmentNum { get; set; }
+        [Required]
+        public RoomNum? Room{ get; set; }
+        public Boolean Signature {
+            
+            set
+            {
+                Signature = false;
+            }
+            get
+            {
+                return Signature;
+            }
+        }
+
+
+
+
+
+
     }
 }
