d0c793a4e82eaabb0a105251325285e126e5f84e
אפרת כהן <efratc66@gmail.com>
2018-05-21 09:50:36 +0300

    Edit fault Form

--- Maonot/Models/FaultForm.cs
+++ Maonot/Models/FaultForm.cs
@@ -11,10 +11,13 @@ namespace Maonot.Models
     public class FaultForm
     {
         public int ID { get; set; }
+        [Required]
         public int Apartment { get; set; }
         public int RoomNum { get; set; }
+        [Required]
         public string FullName { get; set; }
         public string Phone { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
