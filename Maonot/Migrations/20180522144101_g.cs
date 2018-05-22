30fbc49ab64da7fe03d97ec35476692acf0af661
אפרת כהן <efratc66@gmail.com>
2018-05-22 17:53:37 +0300

    bla

---
+++ Maonot/Migrations/20180522144101_g.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Maonot.Migrations
+{
+    public partial class g : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "g",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "G",
+                table: "FaultForms",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "G",
+                table: "FaultForms");
+
+            migrationBuilder.AddColumn<string>(
+                name: "g",
+                table: "Messages",
+                nullable: true);
+        }
+    }
+}
---
+++ Maonot/Migrations/20180522144525_deleteFault.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Maonot.Migrations
+{
+    public partial class deleteFault : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FaultForms");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FaultForms",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Apartment = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    FullName = table.Column<string>(nullable: false),
+                    G = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    RoomNum = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FaultForms", x => x.ID);
+                });
+        }
+    }
+}
